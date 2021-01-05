dfc588b3db0c29df4b06156d4cea0dc0207f6299
pauloabr18 <cnnick.ex@gmail.com>
2021-01-05 17:30:05 -0600

    restador module

--- 03 restador_top.v
+++ 03 restador_top.v
@@ -12,3 +12,13 @@
  *
  * Inspirado por el trabajo del Dr. Neiel I. Leyva Santes.
  * ================================================*/
+
+ module restador_top(
+     input [7:0] data0_i,
+     input [7:0] data1_i,
+     output [7:0] result_o
+ );
+
+ assign result_o = data0_i - data1_i;
+
+ endmodule
