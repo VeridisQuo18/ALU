375c991bdb8a7d841c13961d95b89dceb6a5d966
pauloabr18 <cnnick.ex@gmail.com>
2021-01-05 17:24:42 -0600

    sumador file

---
+++ 02 sumador_top.v
@@ -0,0 +1,14 @@
+/* ================================================
+ * Nombre del proyecto: Una ALU que opera con datos en 8-bit.
+ * Archivo:             02 sumador_top.v (1/...).
+ *
+ * Facultad:            ... de Ingeniería en Electrónica y Comunicaciones.
+ * Universidad:         Universidad Veracruzana.
+ *
+ * Autores:             Paulo Abraham Barrientos Casanova (cnnick.ex@gmail.com),
+ *                      Garrifo Fernández Alexis Alfonso (alexisfdx24@gmail.com),
+ *                      Loyda de León Cristian Armando (loyda182@hotmail.com), y
+ *                      Martagón García Julio César (juliomg458@gmail.com).
+ *
+ * Inspirado por el trabajo del Dr. Neiel I. Leyva Santes.
+ * ================================================*/
