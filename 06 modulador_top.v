09306508871487d1a682558064f30073bb914b6c
pauloabr18 <cnnick.ex@gmail.com>
2021-01-05 17:47:26 -0600

    modulador module

--- 06 modulador_top.v
+++ 06 modulador_top.v
@@ -1,6 +1,6 @@
 /* ================================================
  * Nombre del proyecto: Una ALU que opera con datos en 8-bit.
- * Archivo:             06 modulador_top.v (6/...).
+ * Archivo:             06 modulador_top.v (6/7).
  *
  * Facultad:            ... de Ingeniería en Electrónica y Comunicaciones.
  * Universidad:         Universidad Veracruzana.
