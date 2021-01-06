ce29119989002650e700494207558bae43ad51d1
pauloabr18 <cnnick.ex@gmail.com>
2021-01-05 18:07:38 -0600

    ALU design

--- 00 alu_test.v
+++ 00 alu_test.v
@@ -12,3 +12,77 @@
  *
  * Inspirado por el trabajo del Dr. Neiel I. Leyva Santes.
  * ================================================*/
+
+module alu_test;
+
+
+// Entradas para el modulo a prueba 
+reg  [7:0] data0  ; 
+reg  [7:0] data1  ;
+reg  [2:0] ctrl   ; 
+
+// Salidas del modulo a prueba 
+wire [7:0] result ;
+
+
+// Instanciacion del modulo a prueba
+alu_top alu(
+    .data0_i  ( data0  ), 
+    .data1_i  ( data1  ),
+    .ctrl_i   ( ctrl   ), 
+    .result_o ( result )
+);
+
+// Pruebas
+initial begin
+// Dump waves
+$dumpfile("dump.vcd");
+$dumpvars(1,alu_tb);
+
+// reset - todas las señales de entrada a 0
+$display("-- start simulation --");
+data0 = 4'h0;
+data1 = 4'h0;
+ctrl  = 2'b00;
+
+#50 //delay
+data0 = 4'h6;
+data1 = 4'h2;
+ctrl  = 2'b00;
+$display("Resultado : %h", result  );
+$display("--");
+ 
+#50 //delay
+data0 = 4'h6;
+data1 = 4'h2;
+ctrl  = 2'b01;
+$display("Resultado : %h", result  );
+$display("--");
+ 
+#50 //delay
+data0 = 4'h6;
+data1 = 4'h2;
+ctrl  = 2'b10;
+$display("Resultado : %h", result  );
+$display("--");
+
+#50 //delay
+data0 = 4'h6;
+data1 = 4'h2;
+ctrl  = 2'b11;
+$display("Resultado : %h", result  );
+$display("--");
+
+#50 //delay
+data0 = 4'h6;
+data1 = 4'h2;
+ctrl  = 2'b11;
+$display("Resultado : %h", result  );
+$display("--");
+
+#50 //delay
+$display("-- end simulation --");
+
+end
+
+endmodule
