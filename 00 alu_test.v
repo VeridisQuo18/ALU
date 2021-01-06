3f4bb2bc3a286d9a3087bace448772e536882672
pauloabr18 <cnnick.ex@gmail.com>
2021-01-05 18:49:10 -0600

    ALU test

--- 00 alu_test.v
+++ 00 alu_test.v
@@ -46,39 +46,40 @@ ctrl  = 3'b0;
 data0 = 8'd10;
 data1 = 8'd1;
 ctrl  = 3'b000;
-$display("Resultado de suma: %d", result);
+$display("Resultado inicial: %d", result);
 $display("--");
  
 #50 //delay
 data0 = 8'd20;
 data1 = 8'd10;
 ctrl  = 3'b001;
-$display("Resultado de resta: %d", result);
+$display("Resultado de suma: %d", result);
 $display("--");
  
 #50 //delay
 data0 = 8'd8;
 data1 = 8'd8;
 ctrl  = 3'b010;
-$display("Resultado de producto: %d", result);
+$display("Resultado de resta: %d", result);
 $display("--");
 
 #50 //delay
 data0 = 8'd12;
 data1 = 8'd2;
 ctrl  = 3'b011;
-$display("Resultado de division: %d", result);
+$display("Resultado de multiplicación: %d", result);
 $display("--");
 
 #50 //delay
-data0 = 8'd15;
+data0 = 8'd7;
 data1 = 8'd3;
 ctrl  = 3'b100;
-$display("Resultado de modulo: %d", result);
+$display("Resultado de división: %d", result);
 $display("--");
 
 
 #50 //delay
+$display("Resultado de módulo: %d", result);
 $display("-- Fin de Simulación --");
 end
 
